571491c56dbaa8a1c96a6b6912506f790f0d1191
daniel <sirkaranja12@gmail.com>
2023-07-03 05:16:17 +0300

    updated models file

--- app/models.py
+++ app/models.py
@@ -3,8 +3,34 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
-    __tablename__ = 'Restaurant'
+    __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizzas')
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    restaurants = db.relationship('Restaurant', secondary='restaurant_pizzas')
+
+class Restaurant_pizza(db.Model):
+    __tablename__='restaurant_pizzas'
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant = db.relationship('Restaurant', backref=db.backref('restaurant_pizzas', cascade='all, delete-orphan'))
+    pizza = db.relationship('Pizza', backref=db.backref('restaurant_pizzas', cascade='all, delete-orphan'))
+
+
+
 
 # add any models you may need. 
