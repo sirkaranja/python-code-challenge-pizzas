e316473b02066c54ff2dc3877a20c19ebb6d56c5
daniel <sirkaranja12@gmail.com>
2023-07-03 05:50:01 +0300

    models imported

--- app/app.py
+++ app/app.py
@@ -3,8 +3,7 @@
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
-from models import db, Restaurant
-
+from models import db, Restaurant, Restaurant_pizza, Pizza
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
